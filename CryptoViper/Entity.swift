77d05db5e24a6b550535ce219626031e1e776f6e
Furkan İSLAM <65629988+furkanislam@users.noreply.github.com>
2024-06-26 18:53:51 +0300

    Add files via upload

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Furkan İSLAM on 26.06.2024.
+//
+
+import Foundation
+
+// Struct
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
---
+++ CryptoViper/Presenter.swift
@@ -0,0 +1,49 @@
+//
+//  Presenter.swift
+//  CryptoViper
+//
+//  Created by Furkan İSLAM on 26.06.2024.
+//
+
+import Foundation
+
+
+// Class, protocol
+// Talks to -> interactor, router, view
+
+enum NetworkError : Error {
+    case NetworkFailed
+    case ParsingFailed
+}
+
+protocol AnyPresenter {
+    var router : AnyRouter? {get set}
+    var interactor : AnyInteractor? {get set}
+    var view : AnyView? {get set}
+    
+    func interactorDidDownloadCrypto (result: Result<[Crypto],Error>)
+}
+
+class CryptoPresenter : AnyPresenter {
+    
+    var router: (any AnyRouter)?
+    
+    var interactor: (any AnyInteractor)? {
+        didSet {
+            interactor?.downloadCryptos()
+        }
+    }
+    
+    var view: (any AnyView)?
+    
+    func interactorDidDownloadCrypto(result: Result<[Crypto], any Error>) {
+        switch result {
+        case .success(let cryptos):
+            view?.update(with: cryptos)
+        case .failure(let error):
+            view?.update(with: "Try again later...")
+        }
+    }
+    
+    
+}
